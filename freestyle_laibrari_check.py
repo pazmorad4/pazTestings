39b939d2c6f0c7ac3293de5dc93206ffc4ed9621
paz <pmorad@augury.com>
2022-03-31 16:33:05 +0300

    watta

--- freestyle_laibrari_check.py
+++ freestyle_laibrari_check.py
@@ -1,5 +1,16 @@
-import freestyle as fs
 
-fs.print_eps_paired_to_node()
-print("-------------------Moshe ata lo kipod------------------------")
-fs.print_eps_paired_to_node()
+my_dic = {'shai':{},'iris':{},'ouzi':{}}
+
+getlist = 'iris'
+getlist2=['paz','fkd','paz']
+index = 0x
+list=['shai','iris']
+
+for upperkey in list:
+    for key,value in my_dic[upperkey].items():
+        if upperkey == getlist and key == getlist2[index]:
+            my_dic[upperkey][getlist2] = 1
+
+
+index =+ 1
+print(my_dic)
